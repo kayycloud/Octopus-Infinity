d0b6d1d8676375cfdf144010578129e4d2a1abaf
kayy tamvan <kiijbstore9@gmail.com>
2025-06-25 17:24:09 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 
